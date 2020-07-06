6866ce833eea5a8d5350c92e33b57741fad20ab0
Angelo Fernandes Machado Fonseca <angelofmfonseca@gmail.com>
2020-07-06 19:53:08 -0300

    Main styles example

--- client/src/components/Main/index.tsx
+++ client/src/components/Main/index.tsx
@@ -1,7 +1,15 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h1>Won Games</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo src="/img/logo.svg" alt="logo" />
+    <S.Title>Won Games</S.Title>
+    <S.Description>
+      A e-commerce project for games build with Typescript, React, Next and
+      Styled-Components
+    </S.Description>
+    <S.Illustration src="/img/hero-illustration.svg" alt="main image" />
+  </S.Wrapper>
 )
 
 export default Main
