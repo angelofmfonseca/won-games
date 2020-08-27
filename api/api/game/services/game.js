da36961ebd51f9149befea46229649e9be694f41
Angelo Fernandes Machado Fonseca <angelofmfonseca@gmail.com>
2020-08-27 00:15:31 -0300

    populate service fetch + jsdom

--- api/api/game/services/game.js
+++ api/api/game/services/game.js
@@ -1,7 +1,38 @@
 "use strict";
 
+const axios = require("axios");
+
+const getGameInfo = async (slug) => {
+  const jsdom = require("jsdom");
+  const { JSDOM } = jsdom;
+  const body = await axios.get(`https://www.gog.com/game/${slug}`);
+  const dom = new JSDOM(body.data);
+
+  const ratingElement = dom.window.document.querySelector(
+    ".age-restrictions__icon use"
+  );
+
+  const description = dom.window.document.querySelector(".description");
+
+  return {
+    rating: ratingElement
+      ? ratingElement
+          .getAttribute("xlink:href")
+          .replace(/_/g, "")
+          .replace(/[^\w-]+/g, "")
+      : "Free",
+    short_description: description.textContent.trim().slice(0, 160),
+    description: description.innerHTML,
+  };
+};
+
 module.exports = {
-  populate: (params) => {
-    console.log("Chamando o serviço populate");
+  populate: async (params) => {
+    const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`;
+    const {
+      data: { products },
+    } = await axios.get(gogApiUrl);
+
+    console.log(await getGameInfo(products[1].slug));
   },
 };
