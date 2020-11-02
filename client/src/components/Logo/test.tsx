92279ef5c77d59cb53846fa48fb1aa69e841a21a
Angelo Fernandes Machado Fonseca <angelofmfonseca@gmail.com>
2020-11-02 18:21:33 -0300

    testing Logo normal and large sizes

--- client/src/components/Logo/test.tsx
+++ client/src/components/Logo/test.tsx
@@ -17,4 +17,18 @@ describe('<Logo />', () => {
       color: '#030517'
     })
   })
+
+  it('should render the normal Logo by default', () => {
+    renderWithTheme(<Logo />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '11rem'
+    })
+  })
+
+  it('should render the large Logo when size is passed to the Logo component', () => {
+    renderWithTheme(<Logo size="large" />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '20rem'
+    })
+  })
 })
