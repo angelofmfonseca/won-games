84628adc289acd8fb14f227031d5483b9b232937
Angelo Fernandes Machado Fonseca <angelofmfonseca@gmail.com>
2020-11-18 13:01:49 -0300

    Menu mobile styles + animation

--- client/src/components/Menu/index.tsx
+++ client/src/components/Menu/index.tsx
@@ -5,6 +5,7 @@ import { Search as SearchIcon } from '@styled-icons/material-outlined/Search'
 import { Close as CloseIcon } from '@styled-icons/material-outlined/Close'
 
 import Logo from '../Logo'
+import Button from '../Button'
 import * as S from './styles'
 
 const Menu = () => {
@@ -28,6 +29,19 @@ const Menu = () => {
       </S.MenuGroup>
       <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
         <CloseIcon aria-label="Close Menu" onClick={() => setIsOpen(false)} />
+        <S.MenuNav>
+          <S.MenuLink href="#">Home</S.MenuLink>
+          <S.MenuLink href="#">Explore</S.MenuLink>
+        </S.MenuNav>
+        <S.RegisterBox>
+          <Button fullWidth size="large">
+            Log in
+          </Button>
+          <span>or</span>
+          <S.CreateAccount href="#" title="Sign up">
+            Sign up
+          </S.CreateAccount>
+        </S.RegisterBox>
       </S.MenuFull>
     </S.Wrapper>
   )
