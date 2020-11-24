c8f6f0245e356c4203f207075942d94f157cde78
Angelo Fernandes Machado Fonseca <angelofmfonseca@gmail.com>
2020-11-24 18:57:57 -0300

    Button upgrade - hover + polimorfism

--- client/src/components/Button/test.tsx
+++ client/src/components/Button/test.tsx
@@ -46,4 +46,16 @@ describe('<Button />', () => {
     expect(screen.getByText(/buy/i)).toBeInTheDocument()
     expect(screen.getByTestId('icon')).toBeInTheDocument()
   })
+
+  it('should render button as a link', () => {
+    renderWithTheme(
+      <Button as="a" href="/link">
+        Buy
+      </Button>
+    )
+    expect(screen.getByRole('link', { name: /buy/i })).toHaveAttribute(
+      'href',
+      '/link'
+    )
+  })
 })
