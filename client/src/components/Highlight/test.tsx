813affc7841e2162919bfe1a36814783cec19c1b
Angelo Fernandes Machado Fonseca <angelofmfonseca@gmail.com>
2020-12-11 21:17:50 -0300

    Highlight

--- client/src/components/Highlight/test.tsx
+++ client/src/components/Highlight/test.tsx
@@ -2,13 +2,15 @@ import { screen } from '@testing-library/react'
 import { renderWithTheme } from '../../utils/tests/helpers'
 
 import Highlight from '.'
+import * as S from './styles'
 
 const props = {
   title: 'Heading 1',
   subtitle: 'Heading 2',
   buttonLabel: 'Buy now',
-  buttonLink: '/rdr2',
-  backgroundImage: '/img/red-dead-img.png'
+  buttonLink: '/games/rdr2',
+  backgroundImage: '/img/red-dead-img.png',
+  floatImage: '/red-dead-float.png'
 }
 
 describe('<Highlight />', () => {
@@ -27,6 +29,38 @@ describe('<Highlight />', () => {
     const { container } = renderWithTheme(<Highlight {...props} />)
     expect(container.firstChild).toHaveStyle({
       backgroundImage: `url(${props.backgroundImage})`
+    })
+  })
+
+  it('should render the float image', () => {
+    renderWithTheme(<Highlight {...props} floatImage="/red-dead-float.png" />)
+    expect(screen.getByRole('img', { name: props.title })).toHaveAttribute(
+      'src',
+      '/red-dead-float.png'
+    )
+  })
+
+  it('should render aligned right headings and button by default', () => {
+    const { container } = renderWithTheme(<Highlight {...props} />)
+    expect(container.firstChild).toHaveStyleRule(
+      'grid-template-areas',
+      "'floatimage content'"
+    )
+    expect(container.firstChild).toHaveStyleRule('text-align', 'right', {
+      modifier: `${S.Content}`
+    })
+  })
+
+  it('should render aligned left headings and button if alignment property is passed to the component', () => {
+    const { container } = renderWithTheme(
+      <Highlight {...props} alignment="left" />
+    )
+    expect(container.firstChild).toHaveStyleRule(
+      'grid-template-areas',
+      "'content floatimage'"
+    )
+    expect(container.firstChild).toHaveStyleRule('text-align', 'left', {
+      modifier: `${S.Content}`
     })
   })
 })
